d7f6c28a36599c63a85fa1a1f3e54f27d272ebfc
Hans Trautlein <hans@trautlein.com>
2016-09-30 12:18:58 -0700

    Finished this small repo

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false,
+      above: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({done: !this.state.done});
+  }
+
+  onListItemEnter() {
+    this.setState({above: true})
+  }
+
+  onListItemLeave() {
+    this.setState({above: false})
+  }
+
+  onList
+
+  render() {
+    var style = {fontWeight: this.state.above ? 'bold' : 'normal'};
+
+    return (
+        <li style={style} onMouseOver={this.onListItemEnter.bind(this)} onMouseOut={this.onListItemLeave.bind(this)}>{this.props.item}</li>
+      );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item={item} />)}
+  </ul>
+);
+
+var App = ()  => (
+  <div>
+    <h2>My GroceryList</h2>
+    <GroceryList items={["Cucumbers", "Kale", "Potatoes"]}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
